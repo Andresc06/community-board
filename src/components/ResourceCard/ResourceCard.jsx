eb5d3fee7cddf9b92133d26a2026ba6f56f89515
Andres Contreras Alvarez <andrescontrerasalvarez066@gmail.com>
2026-06-07 21:34:38 -0500

    (ResourceCard.jsx): Add card component for programming sources

--- src/components/ResourceCard/ResourceCard.jsx
+++ src/components/ResourceCard/ResourceCard.jsx
@@ -1,8 +1,25 @@
 const ResourceCard = (props) => {
     return (
-        <>
-            
-        </>
+        <article className="card">
+            <div className="card-img">
+                <img src={props.imageUrl} alt={`${props.title} logo`} className="card-icon" loading="lazy" />
+            </div>
+            <div className="card-body">
+                <p className={`tag tag-${props.catClass}`}>
+                    {props.category}
+                </p>
+                <h2 className="card-title">{props.title}</h2>
+                <p className="card-text">{props.description}</p>
+                <a
+                    href={props.url}
+                    className={`btn btn-${props.catClass}`}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Learn More
+                </a>
+            </div>
+        </article>
     )
 }
 
